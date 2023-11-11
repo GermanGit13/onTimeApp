42c25433a2bd9ef6cd5711122d4c15f7da43c1da
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-11-11 16:28:47 +0100

    Ocultar botones por ROL al inicio

--- app/src/main/java/com/svalero/ontimeapp/view/MainActivity.java
+++ app/src/main/java/com/svalero/ontimeapp/view/MainActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private User user;
     long userId;
     ImageView ivPhotoMenu;
+    Button btListSignsDepartment;
     String photoUrl;
 
     @Override
@@ -47,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
                 .load(photoUrl)
                 .error(R.drawable.notphoto)
                 .into(ivPhotoMenu);
+
+        if (user.getRol().equals("USER")) {
+            btListSignsDepartment = findViewById(R.id.btListSignsDepartment);
+            btListSignsDepartment.setVisibility(View.GONE);
+        }
     }
 }
