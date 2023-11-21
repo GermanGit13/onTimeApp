3a0d3021a6a5fe6c264eaeebb2a8c967fc13a05b
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-11-21 07:27:47 +0100

    Crear busqueda por fecha en listado por user

--- app/src/main/java/com/svalero/ontimeapp/view/SignListByUserView.java
+++ app/src/main/java/com/svalero/ontimeapp/view/SignListByUserView.java
@@ -5,10 +5,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -19,7 +25,9 @@ import com.svalero.ontimeapp.domain.Sign;
 import com.svalero.ontimeapp.domain.User;
 import com.svalero.ontimeapp.presenter.SignListByUserPresenter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class SignListByUserView extends AppCompatActivity implements SignListByUserContract.View {
@@ -31,7 +39,12 @@ public class SignListByUserView extends AppCompatActivity implements SignListByU
     private Bundle bundle; // creamos un bundle para crecoger el objeta extra enviado que esta serializable
     private User user;
     private String userId;
-    private String firstDay;
+    private Button btPickDateByUser;
+    private EditText etPlannedDateByUser;
+    private String firstDay = "";
+    private Button btSearchByUser;
+    private Button btClearByUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +57,23 @@ public class SignListByUserView extends AppCompatActivity implements SignListByU
         bundle = getIntent().getExtras();
         user = (User) bundle.getSerializable("user");
         userId = String.valueOf(user.getId());
-//        firstDay = "2013-11-04";
 
         Log.d("List Sign By User and Day", "Llamada desde view "+ userId + " / " + firstDay); // depurar para ver hasta donde llego
 
         presenter = new SignListByUserPresenter(this); // Instanciamos el presenter y le pasamos el contexto
         presenter.loadSignsByUser(userId, firstDay);
         initializeRecyclerView(); //inicializamos el RecyclerView
+        initializeDatePicker(); //Inicializamos el DatePicker
+
+        btSearchByUser = findViewById(R.id.bt_list_by_user_find);
+        btSearchByUser.setOnClickListener(view -> {
+            findSignsUserByDay();
+        });
+
+        btClearByUser = findViewById(R.id.bt_list_by_user_clear);
+        btClearByUser.setOnClickListener(view -> {
+            resetUserByDay();
+        });
     }
 
     /**
@@ -67,6 +90,49 @@ public class SignListByUserView extends AppCompatActivity implements SignListByU
         recyclerView.setAdapter(adapter);
     }
 
+    /**
+     * Método para el Calendario
+     */
+    private void initializeDatePicker() {
+        etPlannedDateByUser = findViewById(R.id.etPlannedDateByUser);
+        btPickDateByUser = findViewById(R.id.bt_pick_date_list_by_user);
+
+        btPickDateByUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                // on below line we are creating a variable for date picker dialog.
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+                        SignListByUserView.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // on below line we are setting date to our text view.
+                                etPlannedDateByUser.setText(  year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+
+                            }
+                        },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+                // at last we are calling show to
+                // display our date picker dialog.
+                datePickerDialog.show();
+            }
+        });
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -74,6 +140,7 @@ public class SignListByUserView extends AppCompatActivity implements SignListByU
         Log.d("List Sign By User and Day", "Llamada desde view"); // depurar para ver hasta donde llego
         presenter.loadSignsByUser(userId, firstDay);
     }
+
 
     @SuppressLint("NotifyDataSetChanged")
     @Override
@@ -103,5 +170,32 @@ public class SignListByUserView extends AppCompatActivity implements SignListByU
     @Override
     public void showMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void findSignsUserByDay() {
+        SimpleDateFormat fechaFormateada = new SimpleDateFormat("dd-MM-yyyy");
+        firstDay = etPlannedDateByUser.getText().toString();
+        Log.d("List Sign By User and Day", "Fecha del calendario " + firstDay); // depurar para ver hasta donde llego
+
+        presenter.loadSignsByUser(userId,firstDay);
+        adapter.notifyDataSetChanged(); // Notificamos al adapter los cambios
+
+//        new MaterialAlertDialogBuilder(this)
+//                .setTitle(R.string.you_don_t_have_an_department)
+//                .setMessage(R.string.contact_with_administration_depatment)
+//                .setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        presenter.loadAllSings(firstDay);
+//                    }
+//                })
+//                .show();
+        Log.d("List Sign By User and Day", "Llamada desde view showSigns with Day: " + firstDay );
+    }
+
+    public void resetUserByDay() {
+        ((TextView) findViewById(R.id.etPlannedDateByUser)).setText("");
+
+        ((TextView) findViewById(R.id.etPlannedDateByUser)).requestFocus();
     }
 }
