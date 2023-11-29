67b24b36496d861b043c3620053911ea89f94978
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-11-29 22:05:57 +0100

    Conexión con APi onTime en google Cloud

--- app/src/main/java/com/svalero/ontimeapp/api/Constants.java
+++ app/src/main/java/com/svalero/ontimeapp/api/Constants.java
@@ -4,7 +4,8 @@ public class Constants {
     /**
      * URL para si nos conectamos por WIFI con un terminal en la misma WIFI que el ordenador, así accedemos a la API
      */
-    public static String BASE_URL = "http://192.168.1.161:8080/";
+//    public static String BASE_URL = "http://192.168.1.161:8080/";
+    public static String BASE_URL = "https://ontime-406612.ew.r.appspot.com/";
 
 //    /**
 //     * URL para si nos conectamos por el emulador para que acceda a la API en el equipo
